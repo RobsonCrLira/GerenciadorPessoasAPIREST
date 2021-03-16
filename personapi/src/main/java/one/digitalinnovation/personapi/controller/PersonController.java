b3c034bf5e90c709b4f6f180a1de7f7ec3d76291
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-03-16 20:46:44 -0300

    Implementação do buscar e deletar por Id
    Implementação do buscar e deletar por Id

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,26 +1,44 @@
 package one.digitalinnovation.personapi.controller;
 
+
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1/people")
+@RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
-    private PersonService personService;
-
-    public PersonController(PersonService personService){
-        this.personService = personService;
-    }
+    private final PersonService personService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
 
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
 }
